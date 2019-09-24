1fdd7383b3c16f80f0b556ba3d0131b21227a373
dolpsdw <dolpsdw@gmail.com>
2019-09-24 19:38:17 +0200

    Error adding layer

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component, ChangeDetectorRef } from "@angular/core";
-import { tileLayer, latLng, LatLng } from "leaflet";
+import { tileLayer, latLng, LatLng, polyline } from "leaflet";
+declare let L;
+import { AntPath, antPath } from "leaflet-ant-path/dist/leaflet-ant-path.es6";
 
 @Component({
   selector: "app-tab2",
@@ -9,7 +11,8 @@ import { tileLayer, latLng, LatLng } from "leaflet";
 export class Tab2Page {
   options;
   center: LatLng;
-
+  layers: any[];
+  test: any;
   constructor(private cdRef: ChangeDetectorRef) {}
 
   ngOnInit() {
@@ -24,9 +27,18 @@ export class Tab2Page {
       zoom: 15,
       center: this.center
     };
-    this.cdRef.markForCheck();
+    this.test = antPath([this.center, [41.648346, -4.729851]], {
+      use: L.polyline,
+      delay: 400,
+      dashArray: [10, 20],
+      weight: 5,
+      color: "#0000FF",
+      pulseColor: "#FFFFFF"
+    });
+    this.layers = [this.test];
+    //this.cdRef.markForCheck();
   }
-  /*
+  /* https://codehandbook.org/use-leaflet-in-angular/ -> For leaflet.js Plain Injection and L usage
   https://asymmetrik.com/ngx-leaflet-tutorial-angular-cli/
   https://github.com/Asymmetrik/ngx-leaflet/issues/218
   https://www.javascripttuts.com/using-leaflet-open-street-map-in-an-ionic-application-in-one-go/
@@ -34,5 +46,6 @@ export class Tab2Page {
   https://stackoverflow.com/questions/55617065/leaflet-map-doesnt-cover-whole-map-area
   https://github.com/Asymmetrik/ngx-leaflet#getting-a-reference-to-the-map -> for invalidateSize()
   ngZone for changeDetection https://stackoverflow.com/questions/48271492/howto-use-leaflet-in-an-angular-5-app-component
+  https://stackoverflow.com/questions/48271492/howto-use-leaflet-in-an-angular-5-app-component -> More L
   */
 }
